eb9805806378ec1d3b19be22c461d469c5e7bcf8
MicroOptimization <jackiezhen538@gmail.com>
2023-04-13 21:44:35 -0400

    did boundary checking for each of the 4 cardinal directions

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ temp = img.load()
 print(img.getdata())
 """
 
-im = Image.open("test7.png").convert('RGBA')
+im = Image.open("test8.png").convert('RGBA')
 pix_val=list(im.getdata())
 """
 pix_val_flat=[]
@@ -51,8 +51,9 @@ def getMinBounds(bg, img):
     
     img = img.convert('RGBA')
     pixel_map = list(img.getdata())
+    width , height = img.size #img.size returns (width, height) as a tuple
 
-    def formatHorizontally(width): #groups go from Left to Right
+    def formatHorizontally(): #groups go from Left to Right
         ret_me = []
         cur_row = set()
         row_count = 0;
@@ -65,7 +66,7 @@ def getMinBounds(bg, img):
                 row_count = 0;
         return ret_me
     
-    def formatVertically(width):
+    def formatVertically():
         res = [set() for index in range(width)]
         cur_col = 0;
         for i in range(len(pixel_map)):
@@ -74,17 +75,79 @@ def getMinBounds(bg, img):
             if cur_col == width:
                 cur_col = 0;
         return res
-
-    hori_ver = formatHorizontally(img.size[0]) #gets width
     
-    vert_ver = formatVertically(img.size[0])
+    def checkNorth(): #it's inclusive
+        north_bound = 0;
+        for row in hori_ver:
+            if (bg in row and len(row) > 1) or (bg not in row):
+                break
+            else:
+                north_bound += 1;
+        return north_bound
     
+    def checkSouth(): #it's inclusive
+        south_bound = height
+        temp_hori = hori_ver
+        temp_hori.reverse();
+        for row in temp_hori:
+            if (bg in row and len(row) > 1) or (bg not in row):
+                break
+            else:
+                south_bound -= 1;
+        return south_bound
+    
+    def checkWest():
+        west_bound = 0;
+        for row in vert_ver:
+            if (bg in row and len(row) > 1) or (bg not in row):
+                break
+            else:
+                west_bound += 1;
+        return west_bound
 
-    def checkNorth():
-        for i in pixel_map:
-            pass
+    def checkEast(): #it's inclusive
+        east_bound = width
+        temp_vert = vert_ver
+        temp_vert.reverse();
+        for row in vert_ver:
+            if (bg in row and len(row) > 1) or (bg not in row):
+                break
+            else:
+                east_bound -= 1;
+        return east_bound
 
-    return None
+    hori_ver = formatHorizontally()
+    vert_ver = formatVertically()
+    
+    print("North Boundary: " , checkNorth())
+    print("South Boundary: " , checkSouth())
+    print("West Boundary: " , checkWest())
+    print("East Boundary: " , checkEast())
+    
+    minBounds = (checkWest(), checkNorth(), checkEast(), checkSouth())
+    return minBounds
 
 #print(pix_val)
-getMinBounds((0, 0, 0, 0), im)
+getMinBounds((255, 255, 255, 255), im) #returns a tuple of where you crop
+
+
+
+
+xsize, ysize = im.size
+box = (2, 3, 6, 9) #the proper crop for test8.png
+
+temp2 = Image.new('RGBA', im.size)
+cropped = im.crop(box)
+
+temp2.paste(cropped, box)
+
+temp2.show()
+"""
+part1 = im.crop((0, 0, delta, ysize))
+part2 = im.crop((delta, 0, xsize, ysize))
+
+
+temp2 = Image.new('RGBA', im.size)
+
+temp2.paste(part1, (xsize - delta, 0, xsize, ysize))
+temp2.paste(part2, (0, 0, xsize - delta, ysize))"""
